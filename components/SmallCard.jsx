b3419f4b05d8f227bd67eb6cbeebc8be500b9330
Velizar Dabov <velizardabov@gmail.com>
2024-02-24 22:35:16 +0200

    ok

--- components/SmallCard.jsx
+++ components/SmallCard.jsx
@@ -4,7 +4,6 @@ const SmallCard = ({ img, location, distance }) => {
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       <div className="relative h-16 w-16 bg-color-white">
-        {/* left */}
         <Image src={img} layout="fill" className="rounded-lg" alt="explore nearby"/>
       </div>
 
